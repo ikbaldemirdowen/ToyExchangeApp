fba6e4cd7fe14068bc5251403e586b1c28ad3abe
ikbal demirdoven <ikbaldemirdoven@icloud.com>
2022-07-21 21:52:31 -0700

    Initial Commit

---
+++ ToyExchange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToyExchange
+//
+//  Created by Ikbal Demirdoven on 2022-07-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToyExchange/ToyExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToyExchangeApp.swift
+//  ToyExchange
+//
+//  Created by Ikbal Demirdoven on 2022-07-21.
+//
+
+import SwiftUI
+
+@main
+struct ToyExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
